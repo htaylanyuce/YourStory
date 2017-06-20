35ffe2ee20f1847614ba94e07f04d15ed3376ab5
taylan <h.taylanyuce@gmail.com>
2017-06-20 00:56:07 +0000

    landing page is ready

---
+++ blog/v3/config/auth.js
@@ -0,0 +1,11 @@
+ 
+module.exports={
+  "facebook_api_key"      :    '1608779752487747',
+  "facebook_api_secret"   :    'c4f14e177fa1697449c4bdab11347fa1',
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "use_database"          :     "false",
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "",
+  "database"              :     "DB NAME"
+}
